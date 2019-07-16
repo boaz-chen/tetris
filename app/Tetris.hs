ea098615a0a56746db75bad1854f3818bd58ed5b
Boaz Chen <boaz_chen@hotmail.com>
2019-07-16 15:46:08 +0300

    Tetromino rotation

--- app/Tetris.hs
+++ app/Tetris.hs
@@ -6,11 +6,14 @@ import           System.Random
 import           Data.Time.Clock
 import           Data.Time.Format
 import qualified System.Console.ANSI           as Console
+import           Data.Functor
 
-data Tetromino = Tetromino [Block]
+data Tetromino = Tetromino [Block] Offset Size
   deriving (Eq, Show)
 
 type Location = (Int, Int)
+type Offset = (Int, Int)
+type Size = Int
 
 data Block = Block Location Color
   deriving (Eq, Show, Ord)
@@ -18,7 +21,7 @@ data Block = Block Location Color
 type Color = String -- TODO: replace with real Color
 
 cyanI :: Tetromino
-cyanI = Tetromino
+cyanI = fourSizedTetromino
   [ Block (0, 2) "Cyan"
   , Block (1, 2) "Cyan"
   , Block (2, 2) "Cyan"
@@ -26,51 +29,51 @@ cyanI = Tetromino
   ]
 
 yellowO :: Tetromino
-yellowO = Tetromino
-  [ Block (1, 1) "Yellow"
-  , Block (2, 1) "Yellow"
-  , Block (1, 2) "Yellow"
-  , Block (2, 2) "Yellow"
+yellowO = twoSizedTetromino
+  [ Block (0, 0) "Yellow"
+  , Block (1, 0) "Yellow"
+  , Block (0, 1) "Yellow"
+  , Block (1, 1) "Yellow"
   ]
 
 purpleT :: Tetromino
-purpleT = Tetromino
-  [ Block (0, 2) "Purple"
+purpleT = threeSizedTetromino
+  [ Block (0, 1) "Purple"
+  , Block (1, 1) "Purple"
+  , Block (2, 1) "Purple"
   , Block (1, 2) "Purple"
-  , Block (2, 2) "Purple"
-  , Block (1, 3) "Purple"
   ]
 
 greenS :: Tetromino
-greenS = Tetromino
-  [ Block (0, 2) "Green"
+greenS = threeSizedTetromino
+  [ Block (0, 1) "Green"
+  , Block (1, 1) "Green"
   , Block (1, 2) "Green"
-  , Block (1, 3) "Green"
-  , Block (2, 3) "Green"
+  , Block (2, 2) "Green"
   ]
 
 redZ :: Tetromino
-redZ = Tetromino
-  [ Block (0, 3) "Red"
-  , Block (1, 3) "Red"
+redZ = threeSizedTetromino
+  [ Block (0, 2) "Red"
   , Block (1, 2) "Red"
-  , Block (2, 2) "Red"
+  , Block (1, 1) "Red"
+  , Block (2, 1) "Red"
   ]
 
 blueJ :: Tetromino
-blueJ = Tetromino
-  [ Block (0, 3) "Blue"
-  , Block (0, 2) "Blue"
-  , Block (1, 2) "Blue"
-  , Block (2, 2) "Blue"
+blueJ = threeSizedTetromino
+  [ Block (0, 2) "Blue"
+  , Block (0, 1) "Blue"
+  , Block (1, 1) "Blue"
+  , Block (2, 1) "Blue"
   ]
 
 orangeL :: Tetromino
-orangeL = Tetromino
-  [ Block (0, 2) "Orange"
-  , Block (1, 2) "Orange"
+orangeL = threeSizedTetromino
+  [ Block (0, 1) "Orange"
+  , Block (1, 1) "Orange"
+  , Block (2, 1) "Orange"
   , Block (2, 2) "Orange"
-  , Block (2, 3) "Orange"
   ]
 
 tetrominoes :: [Tetromino]
@@ -89,11 +92,10 @@ data BoardState =
 data Board = Board Heap BoardState StdGen
   deriving (Show)
 
-height :: Int
 height = 20
-
-width :: Int
 width = 10
+tetrominoHeight = 4
+tetrominoWidth = 4
 
 startPositionOffset :: Int
 startPositionOffset = height - 3
@@ -102,39 +104,43 @@ data Direction = DirLeft | DirRight | DirDown
 
 -----------
 
+mkTetromino :: Size -> [Block] -> Tetromino
+mkTetromino size blocks = Tetromino blocks (0, 0) size
+
+twoSizedTetromino = mkTetromino 2
+threeSizedTetromino = mkTetromino 3
+fourSizedTetromino = mkTetromino 4
+
+absBlocks :: Tetromino -> [Block]
+absBlocks (Tetromino blocks (xOffset, yOffset) _) =
+  (\(Block (x, y) color) -> (Block (x + xOffset, y + yOffset) color)) <$> blocks
+
 newBoard :: StdGen -> Board
 newBoard gen =
   let (newTetromino, newGen) = randomTetromino gen
-      positionedTetromino    = moveToStartingPosition newTetromino
+      positionedTetromino    = newTetromino
   in  Board emptyHeap (GameOn positionedTetromino) newGen
 
 randomTetromino :: StdGen -> (Tetromino, StdGen)
 randomTetromino gen =
-  let (randomIx, newGen) = randomR (0, (length tetrominoes) - 1) gen
-  in  (tetrominoes !! randomIx, newGen)
-
-moveToStartingPosition :: Tetromino -> Tetromino
-moveToStartingPosition (Tetromino blocks) =
-  let movedBlocks =
-          (\(Block (x, y) color) -> Block (x, y + startPositionOffset) color)
-            <$> blocks
-  in  Tetromino movedBlocks
+  let (randomIx, gen0)               = randomR (0, (length tetrominoes) - 1) gen
+      (Tetromino blocks (_, y) size) = tetrominoes !! randomIx
+      (randomX, gen1)                = randomR (0, width - size - 1) gen
+  in  (Tetromino blocks (randomX, y + startPositionOffset) size, gen1)
 
 offsetTetromino :: Tetromino -> Direction -> Tetromino
-offsetTetromino (Tetromino blocks) dir =
+offsetTetromino (Tetromino blocks (x, y) size) dir =
   let (xOffset, yOffset) = case dir of
         DirLeft  -> (-1, 0)
         DirRight -> (1, 0)
         DirDown  -> (0, -1)
-      movedBlocks =
-          (\(Block (x, y) color) -> (Block (x + xOffset, y + yOffset) color))
-            <$> blocks
-  in  Tetromino movedBlocks
+  in  Tetromino blocks (x + xOffset, y + yOffset) size
 
 moveTetromino :: Board -> Direction -> Board
 moveTetromino board@(Board _ GameOver _) _ = board
 moveTetromino board@(Board heap (GameOn tetromino) gen) dir =
-  let movedTetromino@(Tetromino blocks) = offsetTetromino tetromino dir
+  let movedTetromino = offsetTetromino tetromino dir
+      blocks = absBlocks movedTetromino
       outOfBounds = List.any (\(Block (x, _) _) -> x < 0 || x >= width) blocks
       collidesWithHeap =
           List.any (\(Block location _) -> Map.member location heap) blocks
@@ -144,17 +150,16 @@ moveTetromino board@(Board heap (GameOn tetromino) gen) dir =
         else if landed || collidesWithHeap
           then
             let (newTetromino, newGen) = randomTetromino gen
-            in  (Board (addToHeap tetromino heap)
-                       (GameOn $ moveToStartingPosition newTetromino)
-                       newGen
+            in  (Board (addToHeap tetromino heap) (GameOn $ newTetromino) newGen
                 )
           else (Board heap (GameOn movedTetromino) gen)
 
 addToHeap :: Tetromino -> Heap -> Heap
-addToHeap (Tetromino blocks) heap = List.foldl'
+addToHeap tetromino heap = List.foldl'
   (\acc block@(Block location _) -> Map.insert location block acc)
   heap
   blocks
+  where blocks = absBlocks tetromino
 
 handleBoard :: Board -> Board
 handleBoard b@(Board _    GameOver _  ) = b
@@ -173,21 +178,18 @@ getGoodStdGen = do
   let stdGen = mkStdGen seed
   return stdGen
 
-
+rotate :: Tetromino -> Tetromino
+rotate (Tetromino blocks offset size) = Tetromino (rotateBlock <$> blocks)
+                                                  offset
+                                                  size
+ where
+  rotateBlock :: Block -> Block
+  rotateBlock (Block (x, y) color) = (Block (size - y - 1, x) color)
 
 -- main --
 log' s = putStrLn $ ">>> " <> s
 
 main :: IO ()
--- main = do
---   stdGen <- getGoodStdGen
---   let b = newBoard stdGen
---
---   log' $ "Initial board: " <> show b
---
---   let b' = moveTetromino b DirDown
-
---  log' $ "Move down: " <> show b'
 main = do
   gen <- getGoodStdGen
   loop $ (Board emptyHeap GameOver gen)
@@ -205,33 +207,36 @@ loop board@(Board _ GameOver gen) = do
     'q' -> return board
     _   -> loop board
 
-loop board@(Board heap (GameOn tetromino@(Tetromino blocks)) gen) = do
-  -- putStrLn $ show board
+loop board@(Board heap (GameOn tetromino) gen) = do
   render board
-  log' "r for right. l for left. d for down. q to quit. n to start a new game."
+  log' $ show tetromino
+  log'
+    ". for right. , for left. d for down. space to rotate. q to quit. n to start a new game."
   c <- getChar
 
   case c of
     'n' -> getGoodStdGen >>= (\newGen -> loop $ newBoard newGen)
     'q' -> return board
-    'l' -> loop $ moveTetromino board DirLeft
-    'r' -> loop $ moveTetromino board DirRight
+    ',' -> loop $ moveTetromino board DirLeft
+    '.' -> loop $ moveTetromino board DirRight
     'd' -> loop $ moveTetromino board DirDown
+    ' ' -> loop $ Board heap (GameOn $ rotate tetromino) gen
     _   -> loop board
 
 render :: Board -> IO ()
-render (Board _    GameOver                    _) = putStrLn ""
-render (Board heap (GameOn (Tetromino blocks)) _) = do
+render (Board _    GameOver           _) = putStrLn ""
+render (Board heap (GameOn tetromino) _) = do
   Console.clearScreen
   Console.setCursorPosition 30 0
   let heapBlocks = snd <$> Map.toList heap
+  let blocks     = absBlocks tetromino
 
   mapM_
-    (\(Block (x, y) _) -> Console.setCursorPosition (height - y) x >> putStr "*"
+    (\(Block (x, y) _) -> Console.setCursorPosition (height - y) x >> putStr "o"
     )
     heapBlocks
   mapM_
-    (\(Block (x, y) _) -> Console.setCursorPosition (height - y) x >> putStr "*"
+    (\(Block (x, y) _) -> Console.setCursorPosition (height - y) x >> putStr "o"
     )
     blocks
 
